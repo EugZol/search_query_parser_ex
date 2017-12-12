e5260a5c99e8f9dcce463a946e8801dcaa1e61c7
Eugene Zolotarev <eugzol@gmail.com>
2017-12-12 08:21:33 +0300

    .to_ts_query is customizable

--- lib/search_query_parser_ex.ex
+++ lib/search_query_parser_ex.ex
@@ -5,26 +5,34 @@ defmodule SearchQueryParserEx do
   defdelegate reduce(ast, f), to: SearchQueryParserEx.Ast
   defdelegate to_string(s), to: SearchQueryParserEx.Ast
 
-  def to_ts_query(str, language \\ "english", prefix \\ true) do
+  def to_ts_query(str, prefix \\ true, wrap \\ false, language \\ "english") do
     language = String.replace(language, ~r/[^a-zA-Z]/, "")
     with {:ok, ast} <- parse(str)
     do
-      result =
-        reduce(ast, fn
-          {:" ", x, y} -> ~s{(#{x} <-> #{y})}
-          {:"|", x, y} -> ~s{(#{x} || #{y})}
-          {:"&", x, y} -> ~s{(#{x} && #{y})}
-          {:"!", x}    -> ~s{(!! #{x})}
-          x ->
-            if prefix do
-              ~s{to_tsquery('#{language}', '#{x}:*')}
-            else
-              ~s{to_tsquery('#{language}', '#{x}')}
-            end
-        end)
+      result = reduce(ast, &unwrap(&1, prefix, wrap, language))
       {:ok, result}
     else
       {:error, error} -> {:error, error}
+    end
+  end
+
+  defp op(:" ", _), do: "<->"
+  defp op(atom, _wrap = true), do: op(atom, false) |> String.duplicate(2)
+  defp op(atom, false), do: Kernel.to_string(atom)
+
+  defp unwrap({atom, x, y}, _prefix, wrap, _language), do: ~s{(#{x}#{op(atom, wrap)}#{y})}
+  defp unwrap({atom, x}, _prefix, wrap, _language), do: ~s{(#{op(atom, wrap)}#{x})}
+  defp unwrap(x, prefix, wrap, language) do
+    x =
+      if prefix do
+        x <> ":*"
+      else
+        x
+      end
+    if wrap do
+      ~s{to_tsquery('#{language}', '#{x}')}
+    else
+      x
     end
   end
 end
--- test/search_query_parser_ex_test.exs
+++ test/search_query_parser_ex_test.exs
@@ -3,9 +3,15 @@ defmodule SearchQueryParserExTest do
   doctest SearchQueryParserEx
 
   describe ".to_ts_query/3" do
-    test "produces PostgreSQL ts_query structures from bare string" do
+    test "produces PostgreSQL ts_query structures with wrapping" do
+      {:ok, r} = SearchQueryParserEx.to_ts_query("cats !dogs !собаки || (birds & 'птицы*  ) ^", false, true, "russian")
+      expected = "(((to_tsquery('russian', 'cats')<->(!!to_tsquery('russian', 'dogs')))<->(!!to_tsquery('russian', 'собаки')))||(to_tsquery('russian', 'birds')&&to_tsquery('russian', 'птицы')))"
+      assert r == expected
+    end
+
+    test "produces PostgreSQL ts_query without wrapping" do
       {:ok, r} = SearchQueryParserEx.to_ts_query("cats !dogs !собаки || (birds & 'птицы*  ) ^")
-      expected = "(((to_tsquery('english', 'cats:*') <-> (!! to_tsquery('english', 'dogs:*'))) <-> (!! to_tsquery('english', 'собаки:*'))) || (to_tsquery('english', 'birds:*') && to_tsquery('english', 'птицы:*')))"
+      expected = "(((cats:*<->(!dogs:*))<->(!собаки:*))|(birds:*&птицы:*))"
       assert r == expected
     end
   end
